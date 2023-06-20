77015a2a8a5a480c30d8bb95e45b678a74cf5c89
KhalidRajabov <khalidsr@code.edu.az>
2023-06-21 02:59:00 +0400

    Photostock.api added, views deleted from identity

---
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,49 @@
+﻿using FreeCourse.Services.PhotoStock.DTOs;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+
+            //CANCELLATION TOKEN means if browser is quit or user somehow stopped uploading, 
+            //the processes below will stop as well
+
+
+            if (photo != null&&photo.Length>0) 
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/photos",photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+                var returnPath="photos/"+photo.FileName;
+
+
+                PhotoDTO photoDTO = new() { URL = returnPath };
+                return CreateActionResultInstance(Response<PhotoDTO>.Success(photoDTO,200));
+            }
+            return CreateActionResultInstance(Response<PhotoDTO>.Fail("Photo is empty", 400));
+        }
+
+        [HttpGet]
+        public IActionResult PhotoDelete(string photoURL)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoURL);
+            if(!System.IO.File.Exists(path))
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+            
+            System.IO.File.Delete(path);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+            
+        }
+    }
+}
