2a30300f863516aca5ab1f30d4c37b562182d084
KhalidRajabov <khalidsr@code.edu.az>
2023-06-24 14:33:21 +0400

    order service created

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address:ValueObject
+    {
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string Line { get; set; }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    internal class OrderItem
+    {
+    }
+}
