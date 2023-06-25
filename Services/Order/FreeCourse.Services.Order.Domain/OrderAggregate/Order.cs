9281548009925cc43813c796dc86d3091226c593
KhalidRajabov <khalidsr@code.edu.az>
2023-06-25 12:27:57 +0400

    order picture added

--- Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -7,11 +7,37 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Order.Domain.OrderAggregate
 {
+
     public class Order:Entity,IAggregateRoot
     {
         public DateTime CreatedDate { get; set; }
         public Address Address { get; set; }
         public string BuyerId { get; set; }
         
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        public Order(string buyerId, Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            BuyerId= buyerId;
+            Address= address;
+        }
+
+
+        public void AddOrderItem(string productId, string productName, decimal price, string picUrl) 
+        {
+            var existProduct = _orderItems.Any(x=>x.ProductId==productId);
+            if (!existProduct) 
+            {
+                var newOrderItem = new OrderItem(productId, productName, picUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+
+        //property method, returns decimal, total price of all items in the order
+        public decimal GetTotalPrice=>_orderItems.Sum(x=>x.Price);
     }
 }
---
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Infrastructure
+{
+    public class OrderDbContext:DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options):base(options) { }
+
+        public DbSet<Domain.OrderAggregate.Order> Orders { get; set; }
+        public DbSet<Domain.OrderAggregate.OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //code below asks for table name of entity and schema name
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().Property(x=>x.Price).HasColumnType("decimal(18,2)");
+
+
+            //we can make the code below in a different way, like writing [Owned] on the class name of Address
+            //But then, Domain will understand that it is working with efcore, which is wrong
+            //Because Domain shoul not know what ORM we are using. That is why we write the code below,
+            //meaning that the "Address" class is owned by  Order class
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().OwnsOne(x => x.Address).WithOwner();
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
